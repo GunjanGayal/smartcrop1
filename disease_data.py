b23ce8710971c3f15ca064fe9c67182708e2a13a
Gunjan Gayal <gayalgunjan@gmail.com>
2026-05-02 16:37:43 +0530

    added gitignore

---
+++ disease_data.py
@@ -0,0 +1,364 @@
+disease_info = {
+
+# 🍎 APPLE
+"Apple___Apple_scab": {
+    "prevention": "Remove fallen leaves and prune infected branches.",
+    "treatment": "Spray Captan or Sulfur fungicide.",
+    "severity_hint": "Medium",
+    "season": "Spring",
+    "spread_speed": "Moderate",
+    "crop_impact": "Leaf damage",
+    "organic_option": "Neem oil spray",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Apple___Black_rot": {
+    "prevention": "Remove infected fruits and branches.",
+    "treatment": "Use Mancozeb fungicide.",
+    "severity_hint": "High",
+    "season": "Warm and humid",
+    "spread_speed": "Fast",
+    "crop_impact": "Fruit rot and leaf spots",
+    "organic_option": "Copper spray",
+    "recovery_time": "15-20 days",
+    "infected_part": "Leaves and Fruits"
+},
+
+"Apple___Cedar_apple_rust": {
+    "prevention": "Plant resistant varieties and remove cedar trees.",
+    "treatment": "Apply Myclobutanil fungicide.",
+    "severity_hint": "Medium",
+    "season": "Spring",
+    "spread_speed": "Moderate",
+    "crop_impact": "Leaf damage",
+    "organic_option": "Sulfur spray",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Apple___healthy": {
+    "prevention": "Maintain proper irrigation and fertilization.",
+    "treatment": "No treatment required",
+    "severity_hint": "Healthy condition ",
+    "season": "Suitable for all seasons",
+    "spread": "Not applicable",
+    "crop_impact": "Healthy crop growth with no negative impact",
+    "organic": "Maintain regular organic care practices",
+    "recovery": "Healthy (No recovery needed)",
+    "infected_part": "No infected area found"
+},
+
+# 🫐 BLUEBERRY
+"Blueberry___healthy": {
+    "prevention": "Ensure proper sunlight and watering.",
+     "treatment": "No treatment required",
+    "severity_hint": "Healthy condition ",
+    "season": "Suitable for all seasons",
+    "spread_speed": "Not applicable",
+    "organic_option": "Maintain regular organic care practices",
+    "crop_impact": "Healthy crop growth with no negative impact",
+    "recovery_time": "Healthy (No recovery needed)",
+    "infected_part": "No infected area found"
+},
+
+# 🍒 CHERRY
+"Cherry_(including_sour)___Powdery_mildew": {
+    "prevention": "Maintain air circulation.",
+    "treatment": "Apply sulfur fungicide.",
+    "severity_hint": "Medium",
+    "season": "Dry and warm",
+    "spread_speed": "Fast",
+    "crop_impact": "White powder on leaves",
+    "organic_option": "Baking soda spray",
+    "recovery_time": "7-10 days",
+    "infected_part": "Leaves"
+},
+
+"Cherry_(including_sour)___healthy": {
+    "prevention": "Regular monitoring.",
+    "treatment": "No treatment required",
+    "severity_hint": "Healthy condition",
+    "season": "All",
+    "spread_speed": "No spread",
+    "crop_impact": "Healthy crop growth",
+    "organic_option": "Not required",
+    "recovery_time": "No recovery needed – crop is already healthy",
+    "infected_part": "None"
+},
+
+# 🌽 CORN
+"Corn_(maize)___Northern_Leaf_Blight": {
+    "prevention": "Use resistant hybrids.",
+    "treatment": "Apply Azoxystrobin fungicide.",
+    "severity_hint": "High",
+    "season": "Cool and humid",
+    "spread_speed": "Fast",
+    "crop_impact": "Leaf lesions",
+    "organic_option": "Neem oil spray",
+    "recovery_time": "15-20 days",
+    "infected_part": "Leaves"
+},
+
+"Corn_(maize)___Common_rust_": {
+    "prevention": "Plant resistant varieties.",
+    "treatment": "Use Propiconazole.",
+    "severity_hint": "Medium",
+    "season": "Warm",
+    "spread_speed": "Fast",
+    "crop_impact": "Rust spots",
+    "organic_option": "Garlic extract spray",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot": {
+    "prevention": "Avoid dense planting.",
+    "treatment": "Apply fungicide.",
+    "severity_hint": "High",
+    "season": "Humid",
+    "spread_speed": "Fast",
+    "crop_impact": "Gray lesions",
+    "organic_option": "Neem oil spray",
+    "recovery_time": "15-20 days",
+    "infected_part": "Leaves"
+},
+
+"Corn_(maize)___healthy": {
+    "prevention": "Proper fertilization.",
+        "treatment": "No treatment required",
+        "severity_hint": "Healthy condition",
+        "season": "All",
+        "spread_speed": "No spread",
+        "crop_impact": "Healthy crop growth",
+        "organic_option": "Not required",
+        "recovery_time": "No recovery needed – crop is already healthy",
+        "infected_part": "None"
+    },  
+
+# 🍇 GRAPE
+"Grape___Black_rot": {
+    "prevention": "Remove infected fruits.",
+    "treatment": "Spray Mancozeb.",
+    "severity_hint": "High",
+    "season": "Warm and wet",
+    "spread_speed": "Fast",
+    "crop_impact": "Fruit rot",
+    "organic_option": "Copper spray",
+    "recovery_time": "15-20 days",
+    "infected_part": "Fruits"
+},
+
+"Grape___Esca_(Black_Measles)": {
+    "prevention": "Avoid trunk injuries.",
+    "treatment": "Remove infected vines.",
+    "severity_hint": "High",
+    "season": "Hot",
+    "spread_speed": "Slow",
+    "crop_impact": "Vine death",
+    "organic_option": "Not effective",
+    "recovery_time": "Not recoverable",
+    "infected_part": "Whole plant"
+},
+
+"Grape___Leaf_blight_(Isariopsis_Leaf_Spot)": {
+    "prevention": "Maintain sanitation.",
+    "treatment": "Copper spray.",
+    "severity_hint": "Medium",
+    "season": "Humid",
+    "spread_speed": "Moderate",
+    "crop_impact": "Leaf spots",
+    "organic_option": "Neem oil",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Grape___healthy": {
+    "prevention": "Maintain proper vine training and good air circulation.",
+     "treatment": "No treatment required",
+    "severity_hint": "Healthy condition ",
+    "season": "Suitable for all seasons",
+    "spread_speed": "Not applicable",
+    "organic_option": "Maintain regular organic care practices",
+    "crop_impact": "Healthy crop growth with no negative impact",
+    "recovery_time": "Healthy (No recovery needed)",
+    "infected_part": "No infected area found"
+},
+
+# 🍊 ORANGE
+"Orange___Haunglongbing_(Citrus_greening)": {
+    "prevention": "Control psyllid insects.",
+    "treatment": "Remove infected trees.",
+    "severity_hint": "Very High",
+    "season": "All",
+    "spread_speed": "Fast",
+    "crop_impact": "Tree death",
+    "organic_option": "Neem oil",
+    "recovery_time": "Not recoverable",
+    "infected_part": "Whole plant"
+},
+
+# 🍑 PEACH
+"Peach___Bacterial_spot": {
+    "prevention": "Use disease-free plants.",
+    "treatment": "Copper bactericide.",
+    "severity_hint": "Medium",
+    "season": "Warm and wet",
+    "spread_speed": "Fast",
+    "crop_impact": "Leaf spots",
+    "organic_option": "Copper spray",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Peach___healthy": {
+    "prevention": "Proper care.",
+     "treatment": "No treatment required",
+    "severity_hint": "Healthy condition ",
+    "season": "Suitable for all seasons",
+    "spread_speed": "Not applicable",
+    "organic_option": "Maintain regular organic care practices",
+    "crop_impact": "Healthy crop growth with no negative impact",
+    "recovery_time": "Healthy (No recovery needed)",
+    "infected_part": "No infected area found"
+},
+
+
+# 🌶️ PEPPER
+"Pepper,_bell___Bacterial_spot": {
+    "prevention": "Avoid overhead watering.",
+    "treatment": "Copper spray.",
+    "severity_hint": "Medium",
+    "season": "Warm and humid",
+    "spread_speed": "Fast",
+    "crop_impact": "Leaf damage",
+    "organic_option": "Neem oil",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Pepper,_bell___healthy": {
+    "prevention": "Good irrigation.",
+     "treatment": "No treatment required",
+    "severity_hint": "Healthy condition ",
+    "season": "Suitable for all seasons",
+    "spread_speed": "Not applicable",
+    "organic_option": "Maintain regular organic care practices",
+    "crop_impact": "Healthy crop growth with no negative impact",
+    "recovery_time": "Healthy (No recovery needed)",
+    "infected_part": "No infected area found"
+},
+
+# 🥔 POTATO
+"Potato___Early_blight": {
+    "prevention": "Use crop rotation.",
+    "treatment": "Mancozeb fungicide.",
+    "severity_hint": "Medium",
+    "season": "Warm",
+    "spread_speed": "Moderate",
+    "crop_impact": "Leaf spots",
+    "organic_option": "Neem oil",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Potato___Late_blight": {
+    "prevention": "Avoid moisture.",
+    "treatment": "Metalaxyl fungicide.",
+    "severity_hint": "Very High",
+    "season": "Cool and wet",
+    "spread_speed": "Very Fast",
+    "crop_impact": "Crop destruction",
+    "organic_option": "Not effective",
+    "recovery_time": "Difficult",
+    "infected_part": "Leaves and Tubers"
+},
+
+"Potato___healthy": {
+    "prevention": "Proper irrigation.",
+     "treatment": "No treatment required",
+    "severity_hint": "Healthy condition ",
+    "season": "Suitable for all seasons",
+   "spread_speed": "Not applicable",
+    "organic_option": "Maintain regular organic care practices",
+    "crop_impact": "Healthy crop growth with no negative impact",
+    "recovery_time": "Healthy (No recovery needed)",
+    "infected_part": "No infected area found"
+},
+
+# 🌱 SOYBEAN
+"Soybean___healthy": {
+    "prevention": "Use quality seeds.",
+    "treatment": "No treatment required",
+    "severity_hint": "Healthy condition ",
+    "season": "Suitable for all seasons",
+    "spread": "Not applicable",
+    "crop_impact": "Healthy crop growth with no negative impact",
+    "spread_speed": "Not applicable",
+    "organic_option": "Maintain regular organic care practices",
+    "recovery_time": "Healthy (No recovery needed)",
+    "infected_part": "No infected area found"
+},
+# 🎃 SQUASH
+"Squash___Powdery_mildew": {
+    "prevention": "Avoid overcrowding.",
+    "treatment": "Sulfur fungicide.",
+    "severity_hint": "Medium",
+    "season": "Dry",
+    "spread_speed": "Fast",
+    "crop_impact": "White powder",
+    "organic_option": "Baking soda spray",
+    "recovery_time": "7-10 days",
+    "infected_part": "Leaves"
+},
+
+# 🍅 TOMATO
+"Tomato___Bacterial_spot": {
+    "prevention": "Avoid overhead watering.",
+    "treatment": "Copper spray.",
+    "severity_hint": "High",
+    "season": "Warm and humid",
+    "spread_speed": "Fast",
+    "crop_impact": "Leaf spots",
+    "organic_option": "Neem oil",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Tomato___Early_blight": {
+    "prevention": "Crop rotation.",
+    "treatment": "Mancozeb.",
+    "severity_hint": "Medium",
+    "season": "Warm",
+    "spread_speed": "Moderate",
+    "crop_impact": "Leaf damage",
+    "organic_option": "Neem oil",
+    "recovery_time": "10-15 days",
+    "infected_part": "Leaves"
+},
+
+"Tomato___Late_blight": {
+    "prevention": "Avoid wet leaves.",
+    "treatment": "Metalaxyl.",
+    "severity_hint": "Very High",
+    "season": "Cool and wet",
+    "spread_speed": "Very Fast",
+    "crop_impact": "Severe loss",
+    "organic_option": "Not effective",
+    "recovery_time": "Difficult",
+    "infected_part": "Leaves"
+},
+
+"Tomato___healthy": {
+    "prevention": "Provide adequate sunlight, proper spacing, and regular watering.",
+    "treatment": "No treatment required",
+    "severity_hint": "Healthy condition ",
+    "season": "Suitable for all seasons",
+    "spread_speed": "Not applicable",
+    "organic_option": "Maintain regular organic care practices",
+    "crop_impact": "Healthy crop growth with no negative impact",
+    "recovery_time": "Healthy (No recovery needed)",
+    "infected_part": "No infected area found"
+}
+
+}
