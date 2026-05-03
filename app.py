d48a2cf9d6269d9a3098f3126a475a506c303d1f
Gunjan Gayal <gayalgunjan@gmail.com>
2026-05-03 10:27:23 +0530

    fix: bind to PORT for render

--- app.py
+++ app.py
@@ -333,5 +333,6 @@ def upload():
 
     return render_template("upload.html")
 # ---------------- RUN ----------------
-if __name__=="__main__":
-    app.run(debug=True)
+# Nayi line:
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=False)
